326c699788e1b30e14c0db50fb0ee88c9eeeeaf4
manuelDisacomp <manuel@disacomp.com>
2023-08-10 15:59:03 -0400

    ya actualiza las filas

--- public/js/products.js
+++ public/js/products.js
@@ -1,21 +1,24 @@
-(async() => {
+(async () => {
 
     let productId = document.getElementById('productId');
-    let productName = document.getElementById('productName'); 
+    let productName = document.getElementById('productName');
     let titleProduct = document.getElementById('titleProduct');
     let productDetail = document.getElementById('productDetail');
     let principalImg = document.getElementById('principalImg');
     let principalImgContent = document.getElementById('principalImgContent');
-    
+
     let closeModal = document.getElementById('closeModal');
     let btnClearAll = document.getElementById('btnClearAll');
     let btnSearchProduct = document.getElementById('btnSearchProduct');
     let btnSaveProduct = document.getElementById('btnSaveProduct');
     let modalProduct = document.getElementById('modalProduct');
     let tdobyProduct = document.getElementById('tdobyProduct');
-     
+
     let inputContent = document.getElementById('inputContent');
 
+    let isEditing = false;
+
+    let prodId = 0;
 
     let URL = `${BaseUrl}api/products`;
 
@@ -31,32 +34,36 @@
     let getAllProducts
     try {
         getAllProducts = await fetch(URL)
-        .then(res=>{
-            if(res.status >= 400) throw new Error(`Error al hacer la petición, Error ${res.status}`);
-            return res.json();
-        }) 
-        .catch(err=>{
-            console.log(err)
-        })
-        
+            .then(res => {
+                if (res.status >= 400) throw new Error(`Error al hacer la petición, Error ${res.status}`);
+                return res.json();
+            })
+            .catch(err => {
+                console.log(err)
+            })
+
     } catch (error) {
         showAlertBanner(error)
     }
 
     let getDataForRequest = () => {
-        if (isEditing) {
-            return {
-                description: categoryDescription.value
-            }
-        } else {
-            return {
-                description: categoryDescription.value
-            }
+        return {
+            name: productName.value,
+            title: titleProduct.value,
+            description: productDetail.value
         }
+        // TODO: //usar formdata para hacer el post
+        // let formData = new FormData();
+        // formData.append("image", principalImg.files[0]);
+        // formData.append("name", productName.value);
+        // formData.append("title",  titleProduct.value);
+        // formData.append("description", articleDetail.value);
+        // return formData;
+
     }
 
     let clearAllInputs = () => {
-      
+
         productId.value = 0;
         productName.value = "";
         titleProduct.value = "";
@@ -65,12 +72,15 @@
 
         removeAllDangerAlert(inputContent);
 
+        isEditing = false;
+
     }
 
-    let insertRowOnTable = (dataRow,key) => {
+    let insertRowOnTable = (dataRow, key) => {
         let tr = document.createElement('div');
         tr.classList.add('tr');
         tr.setAttribute('data-key', key);
+        tr.setAttribute('data-id', dataRow.productId);
         td = ` 
             <div class="td text-center">${dataRow.productId}</div>
             <div class="td">${dataRow.name}</div> 
@@ -79,14 +89,19 @@
         return tr;
     }
 
-    let fillTableProducts = (products) => {
-        
-      let fragment = document.createDocumentFragment();
+    let updateRow = () => {
+        let row = tdobyProduct.querySelector(`[data-id="${prodId}"]`);
+        row.querySelector('.td:nth-child(2)').textContent = productName.value;
+    }
 
-      for(let key in products){
+    let fillTableProducts = (products) => {
+
+        let fragment = document.createDocumentFragment();
+
+        for (let key in products) {
 
             let product = products[key];
-            let row = insertRowOnTable(product,key)
+            let row = insertRowOnTable(product, key)
             fragment.append(row);
         }
         tdobyProduct.append(fragment);
@@ -94,99 +109,129 @@
     fillTableProducts(getAllProducts);
 
     let requestUsingFetch = async (method, data) => {
-        
+
         try {
-            return await fetch((method == 'PUT') ? `${URL}/2` : URL, {
+            return await fetch((method == 'PUT') ? `${URL}/${prodId}` : URL, {
                 method: method,
                 body: JSON.stringify(data),
                 headers: {
                     'Content-Type': 'application/json'
                 }
             })
-            .then(res => { 
-                if (res.status >= 400) throw new Error(`Error al hacer ${method}, Error ${res.status}`);
-                return res.text();
-            })
-            .then(async res => {
-                
-                if(method == 'POST'){
-                    res = JSON.parse(res);
-                }
+                .then(res => {
+                    if (res.status >= 400) throw new Error(`Error al hacer ${method}, Error ${res.status}`);
+                    return res.text();
+                })
+                .then(async res => {
 
-                return res;
-            })
-            .catch(err => {
-                throw new Error(`Error al hacer la petición, Error ${res.status}`);
-            })
+                    if (method == 'POST') {
+                        res = JSON.parse(res);
+                    }
+
+                    return res;
+                })
+                .catch(err => {
+                    throw new Error(`Error al hacer la petición, Error ${res.status}`);
+                })
         } catch (error) {
-            showAlertBanner('danger',error);
+            showAlertBanner('danger', error);
         }
     }
 
     let getProductById = async (productId) => {
-        
+
         return await fetch(`${URL}/${productId}`)
             .then(res => {
                 if (res.status >= 400) throw new Error(`Error al hacer la petición, Error ${res.status}`);
                 return res.json();
             })
             .catch(err => {
-                showAlertBanner('danger',err);
+                showAlertBanner('danger', err);
             })
 
     }
 
     let fillAllInputs = (product) => {
-        
+
         productId.value = product.productId;
         productName.value = product.name;
         titleProduct.value = product.title;
         productDetail.value = product.description;
+        prodId = product.productId;
 
     }
 
-    btnSaveProduct.addEventListener('click',async e=>{
-    
+    btnSaveProduct.addEventListener('click', async e => {
+
         let isEmpty = validateInput(inputContent);
 
-        if(isEmpty){
-            showAlertBanner('warning','Faltan parametros');
-            return
+        if (isEmpty) {
+            showAlertBanner('warning', 'Faltan parametros');
+            return;
         }
 
-        let wasAccepted = await showAcceptRejectModal('Crear el producto',`Se creara el producto <span class="strong">${productName.value}</span>`) 
+        let wasAccepted = await showAcceptRejectModal('Guardar el registro', `Si acepta procedera a guardar el registro`)
 
-        if(wasAccepted){
-            showAlertBanner('success','Se ha creado el producto');
+        if (!wasAccepted) {
+            return;   
+        }
+        
+        
+        let data = getDataForRequest();
+        if(isEditing){
+
+            await requestUsingFetch('PUT',data);
+            showAlertBanner('success', 'Se ha editado el producto');
+            updateRow();
             clearAllInputs();
             removeAllDangerAlert(inputContent);
+
+        }else{
+
+            let productCreated = await requestUsingFetch('POST',data);
+            console.log(productCreated);
+            tdobyProduct.append(insertRowOnTable(productCreated,getAllProducts.length));
+            showAlertBanner('success', 'Se ha creado el producto');
+            clearAllInputs();
+            getAllProducts = await fetch(URL)
+            .then(res => {
+                if (res.status >= 400) throw new Error(`Error al hacer la petición, Error ${res.status}`);
+                return res.json();
+            })
+            .catch(err => {
+                console.log(err)
+            })
+            removeAllDangerAlert(inputContent);
+
         }
+
+        isEditing = false;
 
 
     })
 
-    inputContent.addEventListener('keydown',e=> removeDangerAlert(e.target));
-    
+    inputContent.addEventListener('keydown', e => removeDangerAlert(e.target));
+
     btnClearAll.addEventListener('click', clearAllInputs);
 
-    btnSearchProduct.addEventListener('click',e=>{
+    btnSearchProduct.addEventListener('click', e => {
         showModal(modalProduct);
     })
 
-    closeModal.addEventListener('click',e=>{
+    closeModal.addEventListener('click', e => {
         hideModal(modalProduct);
     })
 
-    tdobyProduct.addEventListener('click',async e=>{
+    tdobyProduct.addEventListener('click', async e => {
 
         if (e.target.closest('[data-key]')) {
 
             clearAllInputs();
 
             let key = e.target.closest('[data-key]').getAttribute('data-key');
-            let productById = await getProductById(getAllProducts[key].productId); 
+            let productById = await getProductById(getAllProducts[key].productId);
             fillAllInputs(productById);
-
+            isEditing = true;
             hideModal(modalProduct)
         }
     })
